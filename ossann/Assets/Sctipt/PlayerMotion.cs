0c8a80853c36e50991d4eb498a1efd8209766d98
e145717 <e145717@ie.u-ryukyu.ac.jp>
2016-11-14 18:29:29 +0900

    プレイヤーの移動

---
+++ ossann/Assets/Sctipt/PlayerMotion.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMotion : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKey ("up")) {
+			transform.position += transform.forward * 0.3f;
+		}
+		if (Input.GetKey("right")) {
+			transform.Rotate(0, 10, 0);
+		}
+		if (Input.GetKey ("left")) {
+			transform.Rotate(0, -10, 0);
+		}
+		if (Input.GetKey ("down")) {
+			transform.position -= transform.forward * 0.1f;
+		}
+	}
+}
